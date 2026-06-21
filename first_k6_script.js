1085c83899c7855352c9e4674d3b9edf2a3c9e78
Shivansh Tiwari <stshivansh354@gmail.com>
2026-06-21 16:01:30 +0530

    check and response feature

--- first_k6_script.js
+++ first_k6_script.js
@@ -3,7 +3,12 @@ import {sleep} from 'k6';
 import {htmlReport} from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export default function(){
-    http.get('https://test.k6.io');
+
+    const response = http.get('https://test.k6.io');
+
+    check(response, {
+        'status is 200':(r) => r.status === 200,
+    }),
     sleep(1)
 }
 
