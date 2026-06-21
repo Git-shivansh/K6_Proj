881aaeac91d3c99414c01819343cf29d2a48d2c3
Shivansh Tiwari <stshivansh354@gmail.com>
2026-06-21 16:20:28 +0530

    threshold

--- first_k6_script.js
+++ first_k6_script.js
@@ -1,7 +1,13 @@
 import http from 'k6/http';
-import {sleep} from 'k6';
+import {sleep , check} from 'k6';
 import {htmlReport} from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-
+export const options = {
+    vus : 3,
+    duration : '5s',
+    thresholds : {
+        http_req_duration :['p(95)< 500'],
+    },
+};
 export default function(){
 
     const response = http.get('https://test.k6.io');
